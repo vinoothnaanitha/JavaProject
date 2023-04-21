6defff82f0f4c9158be5b251dc9faadf96bc9957
moode anitha <moode anitha@DESKTOP-U3IDJJT>
2023-04-21 18:53:05 +0530

    anitha

---
+++ AC/src/test/java/com/actitime/testscript/CreateCustomer.java
@@ -0,0 +1,12 @@
+package com.actitime.testscript;
+
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CreateCustomer {
+	@Test
+	public void createcustomer1() {
+		Reporter.log("createcustomer1", true);
+	}		
+}
